1d3bdb56dee86ba9756d5197c64c196c501bfbbd
Travis Whitaker <pi.boy.travis@gmail.com>
2012-08-22 17:10:30 -0400

    Implemented -c, -v, and -p flags, but precision value is not yet used.

--- src/shell.c
+++ src/shell.c
@@ -1,6 +1,7 @@
 //	ChemTerm Copyright (C) 2012 Travis Whitaker
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <gmp.h>
 #include <ptable.h>
 #include <portutils.h>
@@ -9,35 +10,71 @@
 char usersays[256];
 element *output;
 
-int main()
+int main(int argc, char *argv[])
 {
-	printf("ChemTerm v0.0.1d\n");
-	printf("Populating tables...");
-	popPtable();
-	printf("\t\t\t\t\t[OK!]\n");
-	usersays[0] = '\0';
-	while(usersays[0] != 'q' && usersays[0] != 'Q')
+	int version = 0;
+	int precision = 0;
+	int complete = 0;
+	for(int i=0;i<argc;i++)
 	{
-		printf("~#>");
-		fgets(usersays,100,stdin);
-		if(usersays[0] == 'q' || usersays[0] == 'Q')
+		if(portstrcmp(argv[i],"-v",sizeof(argv[i]),3) == 1)
 		{
+			version = 1;
 			break;
 		}
-		else
+		else if(portstrcmp(argv[i],"-c",sizeof(argv[i]),3) == 1)
 		{
-			output = singularParse(muchBetter(usersays,sizeof(usersays)),sizeof(usersays));
-
-			if(output != 0)
+			complete = 1;
+		}
+		else if(portstrcmp(argv[i],"-p",sizeof(argv[i]),3) == 1)
+		{
+			i++;
+			precision = atoi(argv[i]);
+			printf("Got precision level %d\n",precision);
+			return 0;
+		}
+	}
+	if(version == 1)
+	{
+		printf("ChemTerm Version 0.0.1d\n");
+		return 0;
+	}
+	else
+	{
+		printf("Populating tables...");
+		popPtable();
+		printf("\t\t\t\t\t[OK!]\n");
+		usersays[0] = '\0';
+		while(usersays[0] != 'q' && usersays[0] != 'Q')
+		{
+			printf("~#>");
+			fgets(usersays,356,stdin);
+			if(usersays[0] == 'q' || usersays[0] == 'Q')
 			{
-				completeElementData(output);
+				break;
 			}
 			else
 			{
-				printf("No match, try entering an element name or symbol.\n");
+				output = singularParse(muchBetter(usersays,sizeof(usersays)),sizeof(usersays));
+
+				if(output != 0)
+				{
+					if(complete == 1)
+					{
+						completeElementData(output);
+					}
+					else
+					{
+						basicElementData(output);
+					}
+				}
+				else
+				{
+					printf("No match, try entering an element name or symbol.\n");
+				}
 			}
 		}
+		printf("Exiting...");
+		return 0;
 	}
-	printf("Exiting...\n");
-	return 0;
 }
